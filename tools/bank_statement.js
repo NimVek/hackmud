f55c628c58d6f821f42d63521a549f1931ddbb77
Nim'Vek <nimvek@web.de>
2016-10-14 21:58:11 +0200

    minor improvements

--- tools/bank_statement.js
+++ tools/bank_statement.js
@@ -11,7 +11,7 @@ function(context, args) { // count:5
 
     l.each(list, function(_, transaction) {
         transaction.time = l.to_game_timestr(transaction.time);
-        transaction.description = transaction.memo || transaction.script || "";
+        transaction.description = transaction.memo || transaction.script;
         if (transaction.sender == caller) {
             transaction.withdrawal = transaction.amount;
             transaction.opposit = transaction.recipient;
@@ -26,22 +26,22 @@ function(context, args) { // count:5
     });
 
     var titles = [
-        { name: "Date", key: "time" },
-        { name: "Description", key: "description" },
-        { name: "Opposit", key: "opposit" },
-        { name: "Withdrawals", key: "withdrawal", dir: -1, func: d.expandGC },
-        { name: "Deposits", key: "deposit", dir: -1, func: d.expandGC }
+        { name: "+date+", key: "time" },
+        { name: "+description+", key: "description" },
+        { name: "+opposit+", key: "opposit" },
+        { name: "+withdrawals+", key: "withdrawal", dir: -1, func: d.expandGC },
+        { name: "+deposits+", key: "deposit", dir: -1, func: d.expandGC }
     ];
 
     if (balance >= 0) {
         titles.push(
-            { name: "Balance", key: "balance", dir: -1, func: d.expandGC });
+            { name: "+balance+", key: "balance", dir: -1, func: d.expandGC });
     }
 
     var usage = '\n\nFor more transactions, add count:<"all" or number>\nFilter for sender, recipient or script, add from:"<user>", to:"<user>" or script:"<script>".\n';
 
     return {
         ok: true,
-        msg: d.columns(list.reverse(), titles, {}) + usage
+        msg: d.columns(list.reverse(), titles, {pre:"", suf:""}, true) + usage
     };
 }
